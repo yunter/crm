2076c6a75e583bb476bf07f38e28c0b37efefaad
yunter <yunter@gmail.com>
2016-01-07 13:32:48 +0800

    init

--- config.csrf-secret.php
+++
@@ -1,1 +0,0 @@
-<?php $secret = "cba46392d05c49720adcc4c71b6c795a4b33eb6a";
--- modules/ExtensionStore/ExtensionStore.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
-* Copyright (C) www.vtiger.com. All rights reserved.
-* @license Proprietary
-*/
-Class ExtensionStore{
-
-}
-?>
